89555081c4d2b7805309790d23b5c38aedb490c3
BALAJIJFWD <aibalaji657@gmail.com>
2023-12-21 17:48:41 +0530

    first commit

---
+++ src/sampleproject/Main.java
@@ -0,0 +1,14 @@
+package sampleproject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i = 10;
+        while (i <= 50) {
+            System.out.print(i + " ");
+            i++;
+        }
+
+	}
+
+}
